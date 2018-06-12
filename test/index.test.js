8d3b96918d7a9e88334e0ac4ea84f185dc36c5a4
alifaaz <alifaazali@gmail.com>
2018-06-12 03:17:30 +0300

    hello this is me

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ import fs from "fs";
 describe("index.html",()  =>{
   it('sholudsay hello ',(donee) => {
       const index = fs.readFileSync('./src/index.html')
-      jsdom.JSDOM(index ,function(err,window){
+      jsdom.env(index ,function(err,window){
         const h1 = window.getElementsByTagName('h1')[0];
         expect(h1.innerHTM).to.be.equal('Hello World')
         done()
